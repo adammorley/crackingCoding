7f034455d6d6906626ed05af45bdd734ed3291e2
Adam Morley <adam@Adams-Mac-mini.local>
2018-09-17 16:44:41 -0700

    count twos

--- hard.go
+++ hard.go
@@ -5,6 +5,7 @@ import "math/rand"
 import "time"
 
 const oneBits = uint8(8)
+
 // 17.1 in cracking coding book; took about 40 mins
 func one() {
 	var a, b uint8 = 4, 4
@@ -30,20 +31,20 @@ func add(a, b uint8) (r uint8) {
 			r = r | at | bt
 		} else if at&mask == mask && bt&mask == mask {
 			// both one
-            carry = carry << 1
-            r = r & ^mask
+			carry = carry << 1
+			r = r & ^mask
 		} else if carry > 1 {
 			// when carried, how does that manifest?
 			// what happens to carried bits when you have an xor == 1 or both are 1?
 			if at&bt == 0 {
 				// set the bits
-                t := mask
-                for carry != uint8(1) {
-                    carry = carry >> 1
-                    r = r | t
-                    t = t >> 1
-                }
-			    carry = uint8(1)
+				t := mask
+				for carry != uint8(1) {
+					carry = carry >> 1
+					r = r | t
+					t = t >> 1
+				}
+				carry = uint8(1)
 			}
 		}
 	}
@@ -52,29 +53,53 @@ func add(a, b uint8) (r uint8) {
 
 // 15 mins; 17.3 in cracking coding
 func three() {
-    var a []int = []int{5,16,2,9,22}
-    var b []int = []int{3,88,1,4,55,10}
-    fmt.Println(randSet(a, 2), randSet(a, 3), randSet(a, 4))
-    fmt.Println(randSet(b, 4), randSet(b, 2), randSet(b, 3))
+	var a []int = []int{5, 16, 2, 9, 22}
+	var b []int = []int{3, 88, 1, 4, 55, 10}
+	fmt.Println(randSet(a, 2), randSet(a, 3), randSet(a, 4))
+	fmt.Println(randSet(b, 4), randSet(b, 2), randSet(b, 3))
 }
 func randSet(v []int, n int) (r []int) {
-    rand.Seed(time.Now().UnixNano())
-    var m map[int]bool = make(map[int]bool)
-    for i :=0;i<=n;i++{
-        d := rand.Intn(len(v))
-        _, ok := m[d]
-        for ok {
-            d = rand.Intn(len(v))
-            _, ok = m[d]
-        }
-        m[d] = true
-        r = append(r, v[d])
+	rand.Seed(time.Now().UnixNano())
+	var m map[int]bool = make(map[int]bool)
+	for i := 0; i <= n; i++ {
+		d := rand.Intn(len(v))
+		_, ok := m[d]
+		for ok {
+			d = rand.Intn(len(v))
+			_, ok = m[d]
+		}
+		m[d] = true
+		r = append(r, v[d])
+	}
+	return
+}
+
+func six() {
+	if countTwos(25) != 9 {
+		fmt.Println("boo")
+	} else if countTwos(250) != 106 {
+        fmt.Println("boo")
+    } else {
+        fmt.Println("got", countTwos(25), countTwos(250))
     }
-    return
+}
+func countTwos(n int) (r int) {
+	for i := 0; i <= n; i++ {
+		c, j := 0, i
+		for j > 0 {
+			if j%10 == 2 {
+				c++
+			}
+			j = j / 10
+		}
+	    r += c
+	}
+	return
 }
 
 func main() {
 	fmt.Println("hello")
-    one()
-    three()
+	one()
+	three()
+	six()
 }
